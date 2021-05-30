06fb365083df29e3922464cd369b3efacd02c532
vkhnychenko <khnychenko96@gmail.com>
2021-05-30 16:17:53 +0300

    fix app.py

--- app.py
+++ app.py
@@ -1,17 +1,7 @@
-from typing import Optional
-import uvicorn
-from settings import settings
 from api import router
-
-from fastapi import FastAPI, Depends
-from fastapi.security import OAuth2PasswordBearer
-from pydantic import BaseModel
+from fastapi import FastAPI
 
 app = FastAPI()
 app.include_router(router)
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
-
-
 # uvicorn.run('app:app', host=settings.server_host, port=settings.server_port, reload=True)
-
